fe3e8787c011c6f7e28f327a1ea29dad85c58b28
tialles <tialles_zerwes@hotmail.com>
2021-01-18 09:55:19 +1100

    Alterações do Projeto Primeira versão de Produção.

---
+++ assets/js/main.min.js
@@ -0,0 +1,1 @@
+!function(o){var n=o(window),p=o("body");breakpoints({wide:["1281px","1680px"],normal:["981px","1280px"],narrow:["841px","980px"],narrower:["737px","840px"],mobile:["481px","736px"],mobilep:[null,"480px"]}),n.on("load",function(){window.setTimeout(function(){p.removeClass("is-preload")},100)})}(jQuery);
